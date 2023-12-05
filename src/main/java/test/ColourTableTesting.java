e7099c4d1830b6eee20c7cffed8c79f34f83ed15
monarimartina <martinamonari.mm@gmail.com>
2023-12-05 15:00:45 +0000

    Testing for palette size

--- src/main/java/test/ColourTableTesting.java
+++ src/main/java/test/ColourTableTesting.java
@@ -1,5 +1,15 @@
 package test;
+import org.example.ColourTable;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ColourTableTesting {
+    @Test
+    void TestValidPaletteSize(){assertDoesNotThrow(() -> new ColourTable(Size: 4));}
+
+    @Test
+    void TestInvalidPaletteSize(){
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(Size: 3));
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(Size: 1025));
+    }
 }
