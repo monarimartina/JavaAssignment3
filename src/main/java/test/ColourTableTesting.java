8b1281d275cb36e42c512b1734b1d8e7b54796cb
monarimartina <martinamonari.mm@gmail.com>
2023-12-05 17:31:06 +0000

    Testing for Colours

--- src/main/java/test/ColourTableTesting.java
+++ src/main/java/test/ColourTableTesting.java
@@ -5,11 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ColourTableTesting {
     @Test
-    void TestValidPaletteSize(){assertDoesNotThrow(() -> new ColourTable(Size: 4));}
+    void TestValidPaletteSize(){assertDoesNotThrow(() -> new ColourTable(PalSize: 4));}
 
     @Test
     void TestInvalidPaletteSize(){
-        assertThrows(IllegalArgumentException.class, () -> new ColourTable(Size: 3));
-        assertThrows(IllegalArgumentException.class, () -> new ColourTable(Size: 1025));
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(PalSize: 3));
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(PalSize: 1025));
     }
+
+    @Test
+    void TestAddValidColour(){
+        ColourTable colourTable = new ColourTable(PalSize: 4);
+        assertDoesNotThrow(() -> colourTable.add(0xFF0000)); //red
+        assertDoesNotThrow(() -> colourTable.add(0x00FF00)); //green
+        assertDoesNotThrow(() -> colourTable.add(0x0000FF)); //blue
+    }
+
+    @Test
+    void TestAddInvalidColour(){
+        ColourTable colourTable = new ColourTable(PalSize: 4);
+        assertThrows(IllegalArgumentException.class, () -> colourTable.add(0x1234567)); //invalid rgb value
+    }
+
+
 }
