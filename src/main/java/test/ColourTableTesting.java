d260e4d2810037c9984cb2935384c5e14e950b3e
monarimartina <martinamonari.mm@gmail.com>
2023-12-05 17:45:03 +0000

    Testing for multiple and duplicate colours

--- src/main/java/test/ColourTableTesting.java
+++ src/main/java/test/ColourTableTesting.java
@@ -25,7 +25,31 @@ public class ColourTableTesting {
     void TestAddInvalidColour(){
         ColourTable colourTable = new ColourTable(PalSize: 4);
         assertThrows(IllegalArgumentException.class, () -> colourTable.add(0x1234567)); //invalid rgb value
+        assertThrows(IllegalArgumentException.class, () -> colourTable.add(-1)); //negative rgb value
     }
 
+    @Test
+    void TestAddExceedCapacity(){
+        ColourTable colourTable = new ColourTable(PalSize: 2);
+        assertDoesNotThrow(() -> colourTable.add(0xFF0000));
+        assertDoesNotThrow(() -> colourTable.add(0x00FF00));
+        assertThrows(IllegalStateException.class, () -> colourTable.add(0x0000FF));
+    }
+
+    @Test
+    void TestAddMultipleColour(){
+        ColourTable colourTable = new ColourTable(PalSize: 3);
+        assertDoesNotThrow(() -> colourTable.add(0xFF0000));
+        assertDoesNotThrow(() -> colourTable.add(0x00FF00));
+        assertDoesNotThrow(() -> colourTable.add(0x0000FF));
+        assertThrows(IllegalStateException.class, () -> colourTable.add(0xFFFF00));
+    }
+
+    @Test
+    void AddDuplicateColour(){
+        ColourTable colourTable = new ColourTable(PalSize: 4);
+        assertDoesNotThrow(() -> colourTable.add(0xFF0000));
+        assertThrows(IllegalStateException.class, () -> colourTable.add(0xFF0000));
+    }
 
 }
